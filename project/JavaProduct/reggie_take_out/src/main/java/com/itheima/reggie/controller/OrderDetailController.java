714a7e3678ad8dc4e3291f43b98bc01a0158cbe2
test <1418875140@users.noreply.github.com>
2024-01-13 15:54:19 +0800

    瑞吉外卖

---
+++ project/JavaProduct/reggie_take_out/src/main/java/com/itheima/reggie/controller/OrderDetailController.java
@@ -0,0 +1,36 @@
+package com.itheima.reggie.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.dto.DishDto;
+import com.itheima.reggie.dto.OrderDto;
+import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.entity.Orders;
+import com.itheima.reggie.service.OrderDetailService;
+import com.itheima.reggie.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 订单明细
+ */
+@Slf4j
+@RestController
+@RequestMapping("/orderDetail")
+public class OrderDetailController {
+
+    @Autowired
+    private OrderDetailService orderDetailService;
+    @Autowired
+    private OrderService orderService;
+
+
+
+}
